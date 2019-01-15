f247826ee3bd11e590949db440bc37089287c12e
Huỳnh Ngọc Thiên Trang <33373248+thientrang2106@users.noreply.github.com>
2019-01-15 21:32:31 +0700

    update

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 // var http = require('http');
 // var fs = require('fs');
+// var request = require('request');
+
+// var url = 'https://www.amazon.com';
 // http.createServer(function (req, res) {
 //   fs.readFile('popup.html', function(err, data) {
 //     res.writeHead(200, {'Content-Type': 'text/html'});
 //     res.write(data);
+//     request(url, function (err, res, body) 
+// 	{
+// 	    console.log(body);
+// 	    //res.render({html: body});
+// 	})
+    
 //     res.end();
 //   });
 // }).listen(8080);
-
-const cheerio = require('cheerio');
-const request = require('request');
-const nodemailer =  require('nodemailer');
-const utf8 = require('utf8');
-const express = require('express');
-var path = require('path');
+var express = require("express");
 var app = express();
-var fs = require('fs');
-var port = 8080;
-var url = 'https://www.amazon.com/Samsung-Chromebook-Wi-Fi-11-6-Inch-Refurbished/product-reviews/B00M9K7L8S/ref=cm_cr_arp_d_paging_btm_1?ie=UTF8&reviewerType=all_reviews&pageNumber=1';
+var request = require("request");
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.set("views", "./views");
+app.listen(3000);
+app.get("/", function(req, res){
+	request("https://www.amazon.com", function(error, response, body){
+		if(error){
+			console.log(error);
+		}else{
+			//console.log(body);
+			response.setEncoding('utf8');
+			res.render("trangchu",{html:body});
+		}
+	});
+	
+});
 
-request(url, function (err, res, body) 
-{
-    var $ = cheerio.load(body);
-    var newestChap = $('span.a-size-base.review-text');
-    var c = newestChap.text();
-    console.log(c);
-})
-app.listen(port);
-console.log('sever running on' + port);
+
+
+
--- run.js
+++ run.js
@@ -1,13 +1,17 @@
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
- if (request.greeting == "hello"){console.log(request.greeting);
+ if (request.greeting == "hello"){
+ 	console.log(request.greeting);
   
   //alert('hello');
   var tabCurrent = window.location.href;
   console.log(tabCurrent);
   var elements = document.querySelectorAll('span.a-size-base.review-text'); 
   //alert('hello');
-  console.log(elements[0].innerText);
+   var i;
+  for(i = 0; i <  elements.length; i++){
+  	console.log(elements[i].innerText);
+  }
   sendResponse({farewell: tabCurrent}); 
   crawl();
  }
